60d430a58f0802617e18f6044e7df0483c565b7f
brandon-kyle-bailey <brandonkylebailey@outlook.com>
2020-01-14 22:38:35 +0000

    adding greenscreen function and logic

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -24,7 +24,8 @@ function paintToCanvas() {
     return setInterval(() => {
         ctx.drawImage(video, 0, 0, width, height);
         let pixels = ctx.getImageData(0, 0, width, height);
-        pixels = redEffect(pixels);
+        // pixels = redEffect(pixels);
+        pixels = rgbSplit(pixels);
         ctx.putImageData(pixels, 0, 0);
     }, 16);
 }
@@ -60,6 +61,34 @@ function rgbSplit(pixels) {
         pixels.data[i - 150] = pixels.data[i + 2];
 
     }
+    return pixels;
+}
+
+
+function greenScreen(pixels) {
+
+    const levels = {};
+
+    [...document.querySelectorAll('.rgb input')].forEach((input) => {
+        levels[input.name] = input.value;
+    });
+
+    for (let i = 0; i < pixels.data.length; i += 4) {
+        let red = pixels.data[i];
+        let green = pixels.data[i + 1];
+        let blue = pixels.data[i + 2];
+        let alpha = pixels.data[i + 3];
+
+        if (red >= levels.rmin &&
+            green >= levels.gmin &&
+            blue >= levels.bmin &&
+            red <= levels.rmax &&
+            green <= levels.gmax &&
+            blue <= levels.bmax) {
+            alpha = 0;
+        }
+    }
+
     return pixels;
 }
 
