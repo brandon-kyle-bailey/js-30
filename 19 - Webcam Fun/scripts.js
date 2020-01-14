2fa8e3f7e7468a285445582280d1549a18b689dd
brandon-kyle-bailey <brandonkylebailey@outlook.com>
2020-01-14 22:31:30 +0000

    creating rgbsplit function

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -53,6 +53,16 @@ function redEffect(pixels) {
     return pixels;
 }
 
+function rgbSplit(pixels) {
+    for (let i = 0; i < pixels.data.length; i += 4) {
+        pixels.data[i - 150] = pixels.data[i];
+        pixels.data[i + 100] = pixels.data[i + 1];
+        pixels.data[i - 150] = pixels.data[i + 2];
+
+    }
+    return pixels;
+}
+
 
 
 getVideo();
